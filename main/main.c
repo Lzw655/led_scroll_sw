4aa0c2efdabb8f2dc7054f529158fca29b73c84e
lzw655 <liuzhongwei655@gmail.com>
2022-07-31 14:06:06 +0800

    staged project

--- main/main.c
+++ main/main.c
@@ -2,14 +2,21 @@
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "hc595.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "led_7seg_module.h"
+
+static char *TAG = "app_main";
 
 void app_main(void)
 {
+    ESP_LOGI(TAG, "hello world");
+
+    ESP_LOGI(TAG, "Init led_7seg_module");
+    led_7seg_module_init();
+    led_7seg_module_set_display_int(1234);
+
     for (;;) {
-        printf("hello world\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
